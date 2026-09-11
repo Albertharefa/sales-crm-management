9c8b595f73b4608f8d79e040e48ba01af18549a1
Albertharefa <albert.wellkomputindo@gmail.com>
2026-09-11 23:47:46 +0700

    Apply CRM filter styling globally

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,7 @@ import { QueryClientProvider } from '@tanstack/react-query'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import './quotation-print-fix.css'
+import './crm-filter-standard.css'
 import App from './App.tsx'
 import { queryClient } from './lib/queryClient'
 
