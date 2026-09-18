30fc874c526810df104ad886c126d17441ec66b4
Albertharefa <albert.wellkomputindo@gmail.com>
2026-09-18 15:40:43 +0700

    chore: restore Dashboard baseline

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -25,7 +25,7 @@ import { queryClient } from './lib/queryClient'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
+    <QueryClientProvider clientName={undefined}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
